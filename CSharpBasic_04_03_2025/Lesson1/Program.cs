a46952d03df124bc27344037439b1a51be6e6321
vlad <vlad19972017@gmail.com>
2025-03-01 14:06:06 +0200

    Prepare first lesson for basic group.

---
+++ CSharpBasic_04_03_2025/Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson1;
+
+internal class Program
+{
+    // PascalCase
+    static void Main(string[] args)
+    {
+        // https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
+        
+        // camelCase
+        string firstName = "Vlad";
+        string lastName = "Varvashenko";
+
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static void SimpleFunction()
+    {
+        Console.WriteLine("Hello Simple Function/Method");
+    }
+}
