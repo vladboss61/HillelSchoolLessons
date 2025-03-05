ba50388af21abd971ea0c6348ec3b91b5e6753be
vlad <vlad19972017@gmail.com>
2025-03-05 10:57:36 +0200

    Add code from lesson1.

--- CSharpBasic_04_03_2025/Lesson1/Program.cs
+++ CSharpBasic_04_03_2025/Lesson1/Program.cs
@@ -2,14 +2,27 @@
 
 internal class Program
 {
+    // Comment in CSharp.
     // PascalCase
     static void Main(string[] args)
     {
         // https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-        
+
         // camelCase
+
+        //int aget = 1033123;
+
+        //double weight1 = 100.10;
+        //float weight2 = 100.10f;
+
+        //bool isWinter = false;
+        //bool isSpring = true;
+
         string firstName = "Vlad";
         string lastName = "Varvashenko";
+
+        Console.WriteLine(firstName);
+        Console.WriteLine(lastName);
 
         Console.WriteLine("Hello, World!");
     }
