f5cae466aa568288ec278591a6750c13cf20f0f9
vlad <vlad19972017@gmail.com>
2025-03-20 13:46:01 +0200

    Add Lesson 6

---
+++ CSharpBasic_04_03_2025/Lesson6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson6;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            // Loop with 10 iterations.
+            Console.WriteLine($"i: {i}");
+        }
+
+        Console.WriteLine("=====================");
+
+        for (int i = 10; i >= 0; i--)
+        {
+            Console.WriteLine($"Vise-Versa i: {i}");
+        }
+
+        Console.WriteLine("=====================");
+
+        int index = 0;
+        while (true)
+        {
+            Console.WriteLine($"While index: {index}");
+            index++;
+
+            if (index == 25)
+            {
+                break; // вихід з циклу
+            }
+        }
+    }
+}
