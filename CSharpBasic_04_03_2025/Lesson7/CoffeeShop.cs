309ad73828e1e01ba7845d027be6a0bb1594645e
vlad <vlad19972017@gmail.com>
2025-03-22 15:24:26 +0200

    Add Code for lesson 7

---
+++ CSharpBasic_04_03_2025/Lesson7/CoffeeShop.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace Lesson7;
+
+class CoffeeShop
+{
+    private Dictionary<string, double> _menu;
+    private Dictionary<string, int> _cart;
+
+    public CoffeeShop()
+    {
+        _menu = new Dictionary<string, double>
+        {
+            { "Espresso", 2.5 },
+            { "Latte", 3.5 },
+            { "Cappuccino", 3.0 },
+            { "Tea", 2.0 },
+            { "Croissant", 2.5 },
+            { "Muffin", 3.0 }
+        };
+
+        _cart = new Dictionary<string, int>();
+    }
+
+    public void DisplayMenu()
+    {
+        Console.WriteLine("\nМеню кав'ярні:");
+        foreach (var item in _menu)
+        {
+            Console.WriteLine($"{item.Key}: ${item.Value:F2}");
+        }
+    }
+
+    public void AddToCart(string item, int quantity)
+    {
+        if (_menu.ContainsKey(item))
+        {
+            if (_cart.ContainsKey(item))
+            {
+                _cart[item] += quantity;
+            }
+            else
+            {
+                _cart[item] = quantity;
+            }
+            Console.WriteLine($"{quantity} x {item} додано до кошика.");
+        }
+        else
+        {
+            Console.WriteLine("Такого товару немає в меню.");
+        }
+    }
+
+    public void ViewCart()
+    {
+        if (_cart.Count == 0)
+        {
+            Console.WriteLine("Кошик порожній.");
+            return;
+        }
+
+        Console.WriteLine("\nВаш кошик:");
+        double total = 0;
+        foreach (var item in _cart)
+        {
+            double price = _menu[item.Key] * item.Value;
+            total += price;
+            Console.WriteLine($"{item.Key} x {item.Value}: ${price:F2}");
+        }
+        Console.WriteLine($"Загальна сума: ${total:F2}");
+    }
+
+    public void PlaceOrder()
+    {
+        if (_cart.Count == 0)
+        {
+            Console.WriteLine("Ваш кошик порожній. Додайте товари перед оформленням замовлення.");
+            return;
+        }
+
+        ViewCart();
+        Console.WriteLine("Замовлення оформлено! Дякуємо за покупку.");
+        _cart.Clear();
+    }
+}
+
+class StartProgram
+{
+    static void Start()
+    {
+        CoffeeShop shop = new CoffeeShop();
+
+        while (true)
+        {
+            Console.WriteLine("\n1. Переглянути меню");
+            Console.WriteLine("2. Додати до кошика");
+            Console.WriteLine("3. Переглянути кошик");
+            Console.WriteLine("4. Оформити замовлення");
+            Console.WriteLine("5. Вийти");
+            Console.Write("Виберіть опцію: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    shop.DisplayMenu();
+                    break;
+                case "2":
+                    Console.Write("Введіть назву товару: ");
+                    string item = Console.ReadLine();
+                    Console.Write("Введіть кількість: ");
+                    int quantity = int.Parse(Console.ReadLine());
+                    shop.AddToCart(item, quantity);
+                    break;
+                case "3":
+                    shop.ViewCart();
+                    break;
+                case "4":
+                    shop.PlaceOrder();
+                    break;
+                case "5":
+                    Console.WriteLine("Дякуємо, що відвідали нашу кав'ярню!");
+                    return;
+                default:
+                    Console.WriteLine("Невірний вибір, спробуйте ще раз.");
+                    break;
+            }
+        }
+    }
+}
---
+++ CSharpBasic_04_03_2025/Lesson7/Program.cs
@@ -0,0 +1,146 @@
+﻿namespace Lesson7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var result = ConvertInchesToCm_1(10);
+        Console.WriteLine(result);
+    }
+
+    static double ConvertInchesToCm_1(int inches)
+    {
+        const double inchToCm = 2.54;
+        return inches * inchToCm;
+    }
+
+    static void GenerateAndPrintRandomNumbers_2()
+    {
+        Random random = new Random();
+        Console.WriteLine("Випадкові числа:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(random.Next(1, 101));
+        }
+    }
+
+    static void FindNumbersWithSquareSumDivisibleBy13_3()
+    {
+        Console.WriteLine("Числа, сума квадратів цифр яких ділиться на 13:");
+
+        for (int i = 10; i < 100; i++)
+        {
+            int digit1 = i / 10;
+            int digit2 = i % 10;
+            int sumOfSquares = (digit1 * digit1) + (digit2 * digit2);
+
+            if (sumOfSquares % 13 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static void Calculator4()
+    {
+        while (true)
+        {
+            Console.Write("Введіть перше число: ");
+            if (!double.TryParse(Console.ReadLine(), out double num1))
+            {
+                break;
+            }
+
+            Console.Write("Введіть операцію (+, -, *, /): ");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.Write("Введіть друге число: ");
+            if (!double.TryParse(Console.ReadLine(), out double num2))
+            {
+                break;
+            }
+
+            double result = 0;
+            bool validOperation = true;
+
+            switch (operation)
+            {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Помилка: Ділення на нуль!");
+                        validOperation = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Невідома операція!");
+                    validOperation = false;
+                    break;
+            }
+
+            if (validOperation)
+            {
+                Console.WriteLine($"Результат: {result}");
+            }
+
+            Console.WriteLine("Натисніть Enter для продовження або іншу клавішу для виходу...");
+            if (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void CalculateAndDisplayFinalAmount6()
+    {
+        // Введення початкової суми вкладу та кількості місяців
+        Console.Write("Введіть суму вкладу: ");
+        decimal deposit = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Введіть кількість місяців: ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        decimal interestRate = 0.07m; // 7% відсотків на місяць
+
+        // Цикл для нарахування відсотків кожного місяця
+        for (int i = 1; i <= months; i++)
+        {
+            deposit += deposit * interestRate; // Нарахування відсотків
+        }
+
+        // Виведення результату
+        Console.WriteLine($"{Environment.NewLine}Кінцева сума вкладу після {months} місяців: {deposit:F2} грн.");
+    }
+    static void MultiplyNumbers7()
+    {
+        while (true)
+        {
+            Console.Write("Введіть перше число: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber >= 0 && firstNumber <= 10 && secondNumber >= 0 && secondNumber <= 10)
+            {
+                int result = firstNumber * secondNumber;
+                Console.WriteLine($"Результат множення: {result}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неприпустимі числа. Введіть числа в діапазоні від 0 до 10.");
+            }
+        }
+    }
+}
