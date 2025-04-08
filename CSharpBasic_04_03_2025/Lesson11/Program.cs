ffa28d46a671b030ff18440d7c94a88a33cae216
vlad <vlad19972017@gmail.com>
2025-04-08 18:11:21 +0300

    add 1 func

--- CSharpBasic_04_03_2025/Lesson11/Program.cs
+++ CSharpBasic_04_03_2025/Lesson11/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Lesson11;
+﻿using System.Text;
+
+namespace Lesson11;
 
 internal class Program
 {
     public static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
 
         // 1. Підрахунок кількості слів у рядку
         Console.WriteLine("1. Підрахунок кількості слів у рядку");
@@ -13,6 +16,9 @@ internal class Program
         string input1 = Console.ReadLine();
         int wordCount = CountWords(input1);
         Console.WriteLine($"Загальна кількість слів у рядку: {wordCount}");
+
+        // Alternative variant
+        //var length = CountWordsManually("test word ph");
 
         // 2. Вилучення підрядка без бібліотечних функцій
         Console.WriteLine();
@@ -60,6 +66,25 @@ internal class Program
     public static int CountWords(string input)
     {
         return input.Split(" ").Length;
+    }
+
+    public static int CountWordsManually(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return 0;
+        }
+
+        int wordLength = 1;
+        foreach (char symbol in input)
+        {
+            if (symbol == ' ')
+            {
+                wordLength++;
+            }
+        }
+
+        return wordLength;
     }
 
     public static string ExtractSubstring(string input, int start, int length)
@@ -133,6 +158,47 @@ internal class Program
         }
     }
 
+    /*
+     * text.Length = 14
+       substring.Length = 4
+
+       Последняя возможная позиция для начала подстроки: 14 - 4 = 10
+
+       Идём по строке text, начиная с позиции i = 0 до i = 10, и на каждой позиции проверяем,
+       совпадает ли подстрока длиной 4 символа с "test".
+
+    i   Текущий кусок из text (text[i..i+4])	Совпадение с "test"?	count
+    
+    0	text[0..4] = "test"	Да  1
+
+    1   "est "   Нет    1
+    2   "st t"   Нет    1
+    3   "t te"   Нет    1
+    4   " tes"   Нет    1
+    5   "test"   Да     2
+    6   "est "   Нет    2
+    7   "st t"   Нет    2
+    8   "t te"   Нет    2
+    9   " tes"   Нет    2
+    10  "test"   Да     3
+
+
+    text:      t e s t   t e s t   t e s t
+    index:     0 1 2 3 4 5 6 7 8 9 10 11 12 13
+
+    i = 0   [t e s t]   t e s t   t e s t   -> match
+    i = 1     e s t   [ ] t e s t   t e s t -> no
+    i = 2       s t   [ ]   t e s t   t e s -> no
+    i = 3         t   [ ]   t e s t   t e s -> no
+    i = 4           [ ]   t e s t   t e s t -> no
+    i = 5             [t e s t]   t e s t   -> match
+    i = 6               e s t   [ ] e s t   -> no
+    i = 7                 s t   [ ]   s t   -> no
+    i = 8                   t   [ ]   s t   -> no
+    i = 9                     [ ]   s t t   -> no
+    i = 10                      [t e s t]   -> match
+
+     */
     public static int CountSubstringOccurrences(string text, string substring)
     {
         // test test test  14 chars
