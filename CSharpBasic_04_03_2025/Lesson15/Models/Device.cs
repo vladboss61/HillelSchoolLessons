be54c49f72f1475599e0b457e2ecfe9ecda358ba
vlad <vlad19972017@gmail.com>
2025-04-14 21:32:02 +0300

    Update lesson 15

---
+++ CSharpBasic_04_03_2025/Lesson15/Models/Device.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson15.Models;
+
+public abstract class Device
+{
+    public string Name { get; set; }
+
+    protected string Id { get; set; }
+
+    public abstract void Update();
+}
+
+public sealed class IPhone : Device
+{
+    public override void Update()
+    {
+        Console.WriteLine($"Update iPhone ({Name}) from iOS 18 to 19");
+    }
+}
+
+public sealed class Samsung : Device
+{
+    public override void Update()
+    {
+        Console.WriteLine($"Update Samsung ({Name}) from android vanilla ice 15 to baklava 16");
+    }
+}
